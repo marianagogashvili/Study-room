a6586bb9fc2680593b2df4d3d82538478e5ecb93
Mariana Gogashvili <marianagogashvili24@gmail.com>
2021-03-23 22:23:55 +0200

    testwork changed

--- src/app/course/add-article/add-article.component.ts
+++ src/app/course/add-article/add-article.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-add-article',
@@ -6,11 +6,60 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./add-article.component.css']
 })
 export class AddArticleComponent implements OnInit {
+  @ViewChild("container", {static: false}) container: ElementRef;
   @Input() topicId;
   @Input() courseId;
+
+  article ="bt";
   constructor() { }
 
   ngOnInit() {
   }
 
+  add(val) {
+  	const div = document.querySelector('.article');
+  	const ul = document.createElement(val); // ul or ol
+  	var li = document.createElement('li');
+  	ul.appendChild(li);
+
+  	this.container.nativeElement.appendChild(ul);
+  }
+
+  change(val) {
+  	// if (window.getSelection) {
+   //      var sel = window.getSelection();
+   //      if (sel.rangeCount) {
+   //          var range = sel.getRangeAt(0).cloneRange();
+   //          range.surroundContents(val);
+   //          sel.removeAllRanges();
+   //          sel.addRange(range);
+   //      }
+   //  }
+  	const div = document.querySelector('.article');
+
+  	let selection= window.getSelection().getRangeAt(0);
+    let selectedText = selection.extractContents();
+    console.log(selectedText);
+    
+    var bold = document.createElement(val);
+    var div2 = document.createElement('p');
+    bold.appendChild(selectedText);
+    selection.insertNode(bold);
+    selection.insertNode(div2);
+    this.container.nativeElement.appendChild(div2);
+    selection.setStartAfter(bold);
+    selection.collapse(true);
+	window.getSelection().removeAllRanges();
+	window.getSelection().addRange(selection);
+
+	document.getElementById("article").focus();
+  }  	
+
+    // const ul = document.createElement('ul');
+  	// var li = document.createElement('li');
+  	// ul.appendChild(li);
+  	// const ul = document.createElement('b');
+  	// var li = document.createElement('li');
+  	// ul.appendChild(li);
+  	// this.container.nativeElement.appendChild(ul);
 }
--- src/app/course/add-testwork/add-testwork.component.ts
+++ src/app/course/add-testwork/add-testwork.component.ts
@@ -107,6 +107,32 @@ export class AddTestworkComponent implements OnInit, OnDestroy {
   	document.getElementById('create__btn').scrollIntoView({ behavior: 'smooth' });
   }
 
+  onAddChoiceQuestion() {
+    const group = new FormGroup({
+      'title': new FormControl('', Validators.required),
+      'a': new FormControl('', Validators.required),
+      'b': new FormControl('', Validators.required),
+      'c': new FormControl('', Validators.required),
+      'd': new FormControl('', Validators.required),
+      'e': new FormControl('', Validators.required),
+      'f': new FormControl('', Validators.required),
+      'answers': new FormArray([
+        new FormGroup({
+          'a': new FormControl(false, Validators.required),
+          'b': new FormControl(false, Validators.required),
+          'c': new FormControl(false, Validators.required),
+          'd': new FormControl(false, Validators.required),
+          'e': new FormControl(false, Validators.required),
+          'f': new FormControl(false, Validators.required)
+        })
+      ], Validators.required),
+      'points': new FormControl('', Validators.required),
+    });
+    (<FormArray>this.createForm.get('testQuestions')).push(group);
+    document.getElementById('create__btn').scrollIntoView({ behavior: 'smooth' });
+
+  }
+
   onAddQuestion() {
   	const group = new FormGroup({
   		'title': new FormControl('', Validators.required),
@@ -125,42 +151,44 @@ export class AddTestworkComponent implements OnInit, OnDestroy {
  
   saveTestwork() {
   	const testQuestions = this.createForm.value.testQuestions;
-  	if (testQuestions !== []) {
-  		const title = this.createForm.value.title;
-      const tries = this.createForm.value.tries;
-	  	const deadline = this.createForm.value.deadline;
-	  	const hidden = this.createForm.value.hidden;
-	  	const timeRestriction = this.createForm.value.hours * 3600 + this.createForm.value.minutes * 60;
-	  	console.log(this.createForm.value.testQuestions);
+    console.log(testQuestions);
 
-	  	if (this.editTestwork) {
-	  		this.testworkService.updateTestwork({
-		  		testId: this.editTestwork._id,
-		  		title: title, 
-          tries: tries,
-		  		deadline: deadline, 
-		  		hidden: hidden,
-		  		timeRestriction: timeRestriction,
-		  		questions: testQuestions}).subscribe(result => {
-		  			this.router.navigate(['/course/', this.courseId]);
-		  	});
-	  	} else {
-	  		this.testworkService.createTestwork({
-		  		courseId: this.courseId,
-		  		title: title, 
-          tries: tries,
-		  		deadline: deadline, 
-		  		hidden: hidden,
-		  		timeRestriction: timeRestriction,
-		  		topicId: this.topicId,
-		  		questions: testQuestions}).subscribe(result => {
-		  			this.router.navigate(['/course/', this.courseId]);
-		  	});
-	  	}
+  	// if (testQuestions !== []) {
+  	// 	const title = this.createForm.value.title;
+   //    const tries = this.createForm.value.tries;
+	  // 	const deadline = this.createForm.value.deadline;
+	  // 	const hidden = this.createForm.value.hidden;
+	  // 	const timeRestriction = this.createForm.value.hours * 3600 + this.createForm.value.minutes * 60;
+	  // 	console.log(this.createForm.value.testQuestions);
+
+	  // 	if (this.editTestwork) {
+	  // 		this.testworkService.updateTestwork({
+		 //  		testId: this.editTestwork._id,
+		 //  		title: title, 
+   //        tries: tries,
+		 //  		deadline: deadline, 
+		 //  		hidden: hidden,
+		 //  		timeRestriction: timeRestriction,
+		 //  		questions: testQuestions}).subscribe(result => {
+		 //  			this.router.navigate(['/course/', this.courseId]);
+		 //  	});
+	  // 	} else {
+	  // 		this.testworkService.createTestwork({
+		 //  		courseId: this.courseId,
+		 //  		title: title, 
+   //        tries: tries,
+		 //  		deadline: deadline, 
+		 //  		hidden: hidden,
+		 //  		timeRestriction: timeRestriction,
+		 //  		topicId: this.topicId,
+		 //  		questions: testQuestions}).subscribe(result => {
+		 //  			this.router.navigate(['/course/', this.courseId]);
+		 //  	});
+	  // 	}
 	  	
-  	} else {
-  		//show error
-  	}
+  	// } else {
+  	// 	//show error
+  	// }
   }
 
   ngOnDestroy() {
